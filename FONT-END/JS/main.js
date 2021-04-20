c0e9344e5ad0edf14521b206cd1edd4954b7dbef
Tony Gougeon <tony_gougeon@orange.fr>
2021-04-20 21:02:01 +0200

    add main page

---
+++ FONT-END/JS/main.js
@@ -0,0 +1,35 @@
+main()
+
+async function main() {
+    const articles = await getArticles()
+    for (article of articles) {
+        displayArticle(article)
+    }
+}
+
+
+
+function getArticles() {
+    return fetch('http://localhost:3000/api/cameras')
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(articles) {
+            return articles
+        })
+        .catch(function(error) {
+            alert(error)
+        })
+}
+
+function displayArticle(article) {
+    document.getElementById("row").innerHTML += `
+    <div class="card col-md-6 col-lg-4 m-4 shadow" style="width: 22rem;">
+        <img src="${article.imageUrl}" id="img" class="card-img-top" alt="Appareil photo">
+        <div class="card-body">
+            <h5 class="card-title" id="title">${article.title}</h5>
+            <p class="card-text" id="desc">${article.description}</p>
+            <a href="./produit.html?id=${article.id}" class="btn btn-outline-primary" id="href">Voir le produit</a>
+        </div>
+    </div>`
+}
